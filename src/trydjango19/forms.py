cf688532cbf761bf1e94d4307052b1698224fcb4
Chris Kavanagh <ckava3@gmail.com>
2016-02-06 04:25:51 -0500

    fixed stuff

--- src/trydjango19/forms.py
+++ src/trydjango19/forms.py
@@ -1,17 +1,21 @@
 from __future__ import absolute_import
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, ButtonHolder, Submit
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 class UserSignInForm(forms.Form):
+    """login form for user"""
     username= forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     
     
+    
+ # override UserCreationForm and add email. .   
 class RegistrationForm(UserCreationForm):
+    """extending the UserCreationForm"""
     email = forms.EmailField(required=True)        
     
     class Meta:
@@ -23,14 +27,17 @@ class RegistrationForm(UserCreationForm):
             user.email = self.cleaned_data['email']
             if commit:
                 user.save()
-            return user      
+            return user
+            
         
-           
+ 
+# subclass the RegistrationForm (above), use crispy_forms. . 
 class UserRegistrationForm(RegistrationForm):
+    """crispy_forms tag for RegistrationForm"""
     def __init__(self, *args, **kwargs):
-        super(RegistrationForm, self).__init__(*args, **kwargs)
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
         
         self.helper = FormHelper()
-        self.helper.layout = Layout('username', 'email', 'password1', 'password2',
+        self.helper.layout = Layout(Fieldset('Register', 'username', 'email', 'password1', 'password2'),
                                 ButtonHolder(Submit('register','Register',
                                                 css_class='btn-primary')))
